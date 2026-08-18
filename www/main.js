2685514042ad82571f990cb99e22f9023e739bf3
ritesh-crypto-project <rjha15150@gmail.com>
2026-08-18 22:26:41 +0530

    hood design done

---
+++ www/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    $('.text').textllate({
+        loop: true,
+        sync:true,
+        in: {
+            effect: 'bounceIn',
+        },
+        out: {
+            effect: 'bounceOut',
+        }
+    });
+            
+});
